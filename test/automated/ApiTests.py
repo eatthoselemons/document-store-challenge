019579ef4ac892579368088b7528d6aecafc97fc
ethan <preevete@gmail.com>
2021-12-12 16:44:12 -0700

    fixing issue with posting json+file :facepalm:

--- test/automated/ApiTests.py
+++ test/automated/ApiTests.py
@@ -10,43 +10,40 @@ with open(os.path.join(os.getcwd(), '../test-config.json')) as config:
 class BasicApiTests(TestCase):
     def setUp(self) -> None:
         # file 1
-        files = {'upload_file': open('../test-file-1.txt', 'r')}
-        url = baseUrl.concat("upload-file/")
+        url = baseUrl + "upload-file/"
         send_json = {"name": "test-file-1.txt",
                      "folder": "test",
                      "topics": [
                          "first",
                          "second"
                      ]}
-        r = requests.post(url, files=files, json=send_json)
+        r = requests.post(url, json=send_json)
 
         # file 2
 
-        files = {'upload_file': open('../test-file-2.txt', 'r')}
-        url = baseUrl.concat("upload-file/")
+        url = baseUrl + "upload-file/"
         send_json = {"name": "test-file-2.txt",
                      "folder": "test",
                      "topics": [
                          "hello"
                      ]}
-        r = requests.post(url, files=files, json=send_json)
+        r = requests.post(url, json=send_json)
 
         # file 3
 
-        files = {'upload_file': open('../test-file-3.txt', 'r')}
-        url = baseUrl.concat("upload-file/")
+        url = baseUrl + "upload-file/"
         send_json = {"name": "test-file-3.txt",
                      "folder": "other",
                      "topics": [
                          "first",
                          "last"
                      ]}
-        r = requests.post(url, files=files, json=send_json)
+        r = requests.post(url, json=send_json)
         # upload test files
 
-    def get_all_topics(self):
+    def test_get_all_topics(self):
         """get list of every topic in the database"""
-        url = baseUrl.concat("all-topics/")
+        url = baseUrl + "all-topics/"
         r = requests.get(url)
         expected_json = {
             "all topics": [
@@ -57,9 +54,9 @@ class BasicApiTests(TestCase):
         }
         self.assertEqual(r.json, expected_json)
 
-    def search_for_files_by_folder(self):
+    def test_search_for_files_by_folder(self):
         """Searches for all files that are inside a folder that is sent via json"""
-        url = baseUrl.concat("search-folder/")
+        url = baseUrl + "search-folder/"
         search_json = {"folder": "test"}
         r = requests.post(url)
         expected_json = {
@@ -70,9 +67,9 @@ class BasicApiTests(TestCase):
         }
         self.assertEqual(r.json, expected_json)
 
-    def search_for_files_by_topic(self):
+    def test_search_for_files_by_topic(self):
         """Searches for all files that match a topic, topic is sent via json"""
-        url = baseUrl.concat("search-topic/")
+        url = baseUrl + "search-topic/"
         search_json = {"topic": "first"}
         r = requests.post(url)
         expected_json = {
@@ -82,4 +79,3 @@ class BasicApiTests(TestCase):
             ]
         }
         self.assertEqual(r.json, expected_json)
-        pass
--- test/automated/FileApiTests.py
+++ test/automated/FileApiTests.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import TestCase
 import requests
 import json
@@ -8,28 +9,29 @@ with open(os.path.join(os.getcwd(), '../test-config.json')) as config:
 
 
 class FileApiTests(TestCase):
-    def upload_file(self):
+    def test_upload_file(self):
         """uploads a file with dummy data"""
-        files = {'upload_file': open('../test-file-1.txt', 'r')}
-        url = baseUrl.concat("upload-file/")
-        send_json = { "name": "test-file-1.txt",
-                 "folder": "test",
-                 "topics": [
-                     "first",
-                     "second"
-                 ]}
-        r = requests.post(url, files=files, json=send_json)
+        url = baseUrl + "upload-file/"
+        send_json = {"name": "test-file-1.txt",
+                     "folder": "test",
+                     "topics": [
+                         "first",
+                         "second"
+                     ]}
+        r = requests.post(url, json=send_json)
         expected_json = {
             "message": "new file added with name test-file-1.txt"
         }
         self.assertEqual(r.json, expected_json)
 
-    def download_file(self):
+    @unittest.skip("not implemented")
+    def test_download_file(self):
         pass
         # download a file by filename
 
-    def delete_file(self):
-        url = baseUrl.concat("delete-file/")
+    @unittest.skip("not implemented")
+    def test_delete_file(self):
+        url = baseUrl + "delete-file/"
         send_json = {"name": "test-file-1.txt",
                      "folder": "test",
                      }
@@ -40,4 +42,3 @@ class FileApiTests(TestCase):
         self.assertEqual(r.json, expected_json)
         pass
         # delete a file by filename and folder
-
