f544a105b088e7010e1086a5412d568554c6757b
ethan <preevete@gmail.com>
2021-12-12 01:06:40 -0700

    added apiTest skeletons

---
+++ test/automated/ApiTests.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+import os
+
+
+class BasicApiTests(TestCase):
+    def setUp(self) -> None:
+        # upload test files
+
+    def get_all_topics(self):
+        # test the search for all topics
+
+    def search_for_files_by_folder(self):
+        # search all files based on folder
+
+    def search_for_files_by_topic(self):
+        # search all files, and filter by topic
---
+++ test/automated/FileApiTests.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+import os
+
+
+class FileApiTests(TestCase):
+    def upload_file(self):
+        # uploads a random file
+
+    def download_file(self):
+        # download a file by filename
+
